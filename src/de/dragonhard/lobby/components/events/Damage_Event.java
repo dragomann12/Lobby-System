5cac2d9d6c9fed43d33eb45c745522f9df40c4a9
Dragonhard117 <dragonhard117@chaos-craft.de>
2020-12-01 11:19:33 +0100

    add Damage_Event now the Player cant die anymore

--- src/de/dragonhard/lobby/components/events/Damage_Event.java
+++ src/de/dragonhard/lobby/components/events/Damage_Event.java
@@ -1,17 +1,20 @@
 package de.dragonhard.lobby.components.events;
 
+import net.minecraft.server.v1_8_R3.EntityFallingBlock;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 
 public class Damage_Event implements Listener {
 
+    @EventHandler
     public void onPlayerDamage(EntityDamageEvent e){
         if(e instanceof Player){
             Player p = (Player) e.getEntity();
             e.setCancelled(true);
         }
-
-
     }
+
+
 }
