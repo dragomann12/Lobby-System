6c990306f0e312eea494219b61f270f7713b3aa1
Dragonhard117 <dragonhard117@chaos-craft.de>
2020-12-07 19:31:33 +0100

    added Function to create a Server wide Message

--- src/de/dragonhard/lobby/manager/ServerMessageManager.java
+++ src/de/dragonhard/lobby/manager/ServerMessageManager.java
@@ -6,6 +6,7 @@ import de.dragonhard.lobby.reader.MessageReader;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class ServerMessageManager extends MessageReader {
@@ -14,22 +15,23 @@ public class ServerMessageManager extends MessageReader {
 
     public void sendMessage(String name){
 
-        String color = this.getString("color");
-        String nameColor = this.getString("color");
+        String messageStart = this.getString("StartColor");
+        String nameColor = this.getString("TitleColor");
         Boolean isImportant = this.getBoolean("isImportant");
         String messageName = this.getString("MessageName");
         String messageColor = this.getString("MessageColor");
         String message = "§" + messageColor + this.getString("Message");
 
-        if(isImportant){color = "4";}
+        if(isImportant){messageStart = "4";}
 
-        String title = "§" + nameColor + messageName + " §" + color;
+        String title = "§" + messageStart + "++ §" + nameColor + messageName + " §" + messageStart + "++";
 
         for(Player p :Bukkit.getOnlinePlayers()){
+
             if(cm.isDebugMode()){
                 ConsoleWriter.writeDebug("loading online Player ...");
             }
-            
+
             PluginWithlistManager pwm = new PluginWithlistManager();
             if (pwm.isOwner(p) || pwm.isTeam_lead(p) || pwm.isDeveloper(p) || pwm.isBuilder(p)) {
 
@@ -38,6 +40,28 @@ public class ServerMessageManager extends MessageReader {
             }
         }
 
+    }
+
+    public void createMessage(Player p,String name, String messageStart, String titleColor, String messageColor, String message, boolean isImportant){
+        if(messageExists(name)){p.sendMessage("§4");}
+        this.setFile(p,name);
+
+        this.setDefault("StartColor", messageStart);
+        this.setDefault("TitleColor", titleColor);
+
+        this.setDefault("MessageColor", messageColor);
+        this.setDefault("MessageName", name);
+
+        this.setDefault("isImportant", isImportant);
+        this.setDefault("Message", message);
+
+    }
+
+    public boolean messageExists(String msgName){
+        File f = this.getFile(msgName);
+
+        if(f.exists()){return true;}
+        return false;
     }
 
 }
