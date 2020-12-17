5889c42d9d7f814034296c9a45dd9e4d7eb31aca
Dragonhard117 <dragonhard117@chaos-craft.de>
2020-12-17 20:23:54 +0100

    edits at the CoinManager

--- src/de/dragonhard/lobby/manager/CoinManager.java
+++ src/de/dragonhard/lobby/manager/CoinManager.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 
 public class CoinManager extends PlayerConfigManager {
  private static int maxCoins = 1000000;
+ private static final int defaultValue_Coins = 0;
 
 public void addCoins(Player p, int value){
     if(this.getCoins(p) == maxCoins) {return;}
@@ -11,11 +12,32 @@ public void addCoins(Player p, int value){
     getMessage(p,"add", value);
 }
 
+public void removeCoins(Player p, int value){
+if(this.getCoins(p)< value) {
+    getMessage(p,"noMoney",0);
+    return;
+}
+    this.setCoins(p,this.getCoins(p) - value);
+    getMessage(p,"add", value);
+
+}
+
+    public void setCoins(Player p, int value){
+        this.setCoins(p, value);
+        getMessage(p,"add", value);
+    }
+
+    public int getCurrentCoins(Player p){
+    return this.getCoins(p);
+    }
+
 public void getMessage(Player p, String type, int value){
 
     switch(type){
 
         case "add": p.sendMessage("§bDu hast §a" + value + " CC §bbekommen du hast jetzt §a" + this.getCoins(p) + " CC");break;
+        case "buy": p.sendMessage("§bDu hast §a" + value + " CC §bgezahlt");break;
+        case "noMoney": p.sendMessage("§4Deine Chaos-Coins(CC) reichen nicht!");break;
         case "remove": p.sendMessage("§bDu hast §e" + value + " CC §bverloren du hast jetzt §e" + this.getCoins(p) + " CC");break;
         case "set": p.sendMessage("§bDeine Chaos-Coins wurden auf §a" + this.getCoins(p) + " CC §bgesetzt");break;
 
